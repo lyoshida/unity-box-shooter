0bbe77a5c760c5b6d0332698d4276050213a32ee
Luis Yoshida <luis.yoshida@gmail.com>
2015-10-20 14:49:06 -0200

    Added a basic controller to the player.

---
+++ Assets/Scripts/BasicController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Horizontal Input = " + Input.GetAxis ("Horizontal"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
